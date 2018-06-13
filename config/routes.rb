92e85d856d6eb18c4dfcf98b94ac11e285756795
absar-mushtaq <absar.mushtaq@ispokepakistan.com>
2018-06-13 14:18:21 +0500

    login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # get 'home/index'
+
+  root 'home#index'
+  resources :home, only: :index do
+  	collection do 
+	  	post :redirect
+	  end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
